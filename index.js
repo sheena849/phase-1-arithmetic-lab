d014c94cf8860ae1bb320e9656eb5d364dc3673b
sheena849 <sheenamugo04@gmail.com>
2024-09-23 14:11:33 +0300

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 // Write your code here
+let random = 25;
+let num3 = 12;
+let num4 = 8;
+let mod = num3%num4;
+let max = (4,8,12,16,20);
